ff4c5b881e54555a152cd2e232c6df2e17b89b47
Tajudeen <lwltjdn@gmail.com>
2023-12-27 21:16:50 +0100

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,66 +1,73 @@
 #include "sort.h"
 
+/**
+ * swap_items - Swaps two items in an array.
+ * @array: The array to modify.
+ * @l: The index of the left item.
+ * @r: The index of the right item.
+ */
+void swap_items(int *array, size_t l, size_t r)
+{
+	int tmp;
+
+	if (array != NULL)
+	{
+		tmp = array[l];
+		array[l] = array[r];
+		array[r] = tmp;
+	}
+}
 
 /**
- * quick_sort - Entry point to quick sort
- * @array: the array
- * @size: the array size
+ * quick_sort_range_lomuto - Sorts a sub array using the
+ * quick sort algorithm and Lomuto's partition scheme.
+ * @array: The array containing the sub-array.
+ * @low: The starting position of the sub-array.
+ * @high: The ending position of the sub-array.
+ * @size: The length of the array.
+ */
+void quick_sort_range_lomuto(int *array, size_t low, size_t high, size_t size)
+{
+	size_t k, i;
+	int pivot;
+
+	if ((low >= high) || (array == NULL))
+		return;
+	pivot = array[high];
+	k = low;
+	for (i = low; i < high; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			if (k != i)
+			{
+				swap_items(array, k, i);
+				print_array(array, size);
+			}
+			k++;
+		}
+	}
+	if (k != high)
+	{
+		swap_items(array, k, high);
+		print_array(array, size);
+	}
+	if (k - low > 1)
+		quick_sort_range_lomuto(array, low, k - 1, size);
+	if (high - k > 1)
+		quick_sort_range_lomuto(array, k + 1, high, size);
+}
+
+/**
+ * quick_sort - Sorts an array using the quick sort algorithm
+ * and Lomuto's partition scheme.
+ * @array: The array to sort.
+ * @size: The length of the array.
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!(array == NULL))
+	if (array != NULL)
 	{
-		quick_sort_lomuto(array, 0, size - 1, size);
-	} /* End if */
-} /* END FUNCTION */
-
-/**
- * quick_sort_lomuto - Entry point
- * @array: 'The array'
- * @low: 'The starting point'
- * @high: 'The end point'
- * @size: 'The size of array'
- */
-void quick_sort_lomuto(int *array, size_t low, size_t high, size_t size)
-{
-	size_t k, i;
-	int pivot, temp; /* The pivote */
-
-	/* Validate parameters */
-	if ((low >= high) || (array == NULL))
-		return;
-	/* Choose the pivot element. */
-	pivot = array[high];
-	k = low;
-	/* Iterate through the elements from low to high - 1 */
-	for (i = low; i <= high - 1; i++)
-	{
-		/* If the current element is <= to the pivot, swap */
-		if (array[i] <= pivot)
-		{ /* Swap array[k] && array[i] */
-			temp = array[k];
-			array[k] = array[i];
-			array[i] = temp;
-
-			print_array(array, size);
-			k++;
-		} /* End if */
-	} /* end for */
-	/*
-	 * Swap the pivot element with the element at k
-	 * to place the pivot in its correct sorted position.
-	 */
-	if (k != high)
-	{ /* Swap array[k] && array[high] */
-		temp = array[k];
-		array[k] = array[high];
-		array[high] = temp;
-
-		print_array(array, size);
-	} /* end if */
-	/* Recursively sort the subarrays to the left and right of the pivot. */
-	if (k - low > 1)
-		quick_sort_lomuto(array, low, k - 1, size);
-	if (high - k > 1)
-		quick_sort_lomuto(array, k + 1, high, size);
-} /* End function */
+		quick_sort_range_lomuto(array, 0, size - 1, size);
+	}
+}
